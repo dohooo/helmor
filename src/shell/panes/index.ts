d7e911a0df83cca3e8db0334341161c5a260f12c
Qui Luong <jerry.luong.94@gmail.com>
2026-06-08 11:48:12 -0400

    feat(panes): export useSyncDefaultPaneToSelection from barrel

--- src/shell/panes/index.ts
+++ src/shell/panes/index.ts
@@ -1,6 +1,6 @@
 // src/shell/panes/index.ts
 export { PaneIdentityContext, usePaneIdentity } from "./identity-context";
-export { PanesProvider, usePanes } from "./provider";
+export { PanesProvider, usePanes, useSyncDefaultPaneToSelection } from "./provider";
 export type { PanesContextValue, PanesState } from "./provider";
 export { PaneErrorBoundary } from "./error-boundary";
 export { PaneShell } from "./pane-shell";
